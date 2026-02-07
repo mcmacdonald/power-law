cacc0321c633bb8c1dd30af737e6826253f455b8
Mitch Macdonald <35157616+mcmacdonald@users.noreply.github.com>
2026-02-07 11:43:21 -0400

    update

--- 02.R
+++ 02.R
@@ -24,37 +24,37 @@ output <- function(filename, figure, path = path, width = 10, height = 5){
 
 
 
-# rank order the market cap by sector
+# rank order the holdings by sector
 equities <- equities %>%
   dplyr::group_by(Sector) %>%
-  dplyr::mutate(Rank = dplyr::dense_rank(dplyr::desc(aum))) %>% # rank order
+  dplyr::mutate(Rank = dplyr::dense_rank(dplyr::desc(holdings))) %>% # rank order
   dplyr::mutate(Case = dplyr::row_number()) %>% # case number for sorting
   dplyr::ungroup() %>%
   dplyr::arrange(Sector, Case, Rank) # sort
 
-# calculate quantiles for the cumulative distribution function
+# calculate quantiles for the cumulative distribution function for S&P 1,500 composite equity index
 sp1500 <- equities %>%
   dplyr::mutate(
-    q50 = stats::quantile(aum, probs = 0.50), # calculate size of 50% of firms 
-    q80 = stats::quantile(aum, probs = 0.80) # calculate size of 80% of firms
+    q50 = stats::quantile(holdings, probs = 0.50), # 50% of holdings
+    q80 = stats::quantile(holdings, probs = 0.80) # 80% of holdings
     )
 
-# the cumulative distribution of firm size for S&P 1,500 composite equity index
-fig01 <- ggplot2::ggplot(sp1500, ggplot2::aes(aum)) +
+# the cumulative distribution of holdings for S&P 1,500 composite equity index
+fig01 <- ggplot2::ggplot(sp1500, ggplot2::aes(holdings)) +
   ggplot2::stat_ecdf(geom = "step", linewidth = 1, col = "black", alpha = 1.00) +
   ggplot2::theme_bw() +
   ggplot2::theme(text = ggplot2::element_text(size = 12)) +
   ggplot2::scale_x_log10() +
   ggplot2::scale_y_continuous(labels = scales::percent) +
-  ggplot2::xlab("(Logged) Assets under management S&P 1,500 Composite, Billions (USD)") +
+  ggplot2::xlab("(Logged) Holdings in S&P 1,500 Composite, Billions (USD)") +
   ggplot2::ylab("Empiricial Cumulative Distribution Function (ECDF)") +
-  # mark 50% of firms
+  # mark 50% of holdings
   ggplot2::geom_vline(data = sp1500, ggplot2::aes(xintercept = q80), linewidth = 1, linetype = "dashed", color = "red") +
-  # mark 80% of firms
+  # mark 80% of holdings
   ggplot2::geom_vline(data = sp1500, ggplot2::aes(xintercept = q50), linewidth = 1, linetype = "dashed", color = "red") +
   # label the lines
-  ggplot2::geom_label(data = sp1500, mapping = ggplot2::aes(x = q50, y = 0.25, label = paste("\u2264 50% of firms:\n", round(q50, digits = 2), "B")), size = 6/ggplot2::.pt) + 
-  ggplot2::geom_label(data = sp1500, mapping = ggplot2::aes(x = q80, y = 0.50, label = paste("\u2264 80% of firms:\n", round(q80, digits = 2), "B")), size = 6/ggplot2::.pt) 
+  ggplot2::geom_label(data = sp1500, mapping = ggplot2::aes(x = q50, y = 0.25, label = paste("\u2264 50% of holdings:\n", round(q50, digits = 2), "B")), size = 6/ggplot2::.pt) + 
+  ggplot2::geom_label(data = sp1500, mapping = ggplot2::aes(x = q80, y = 0.50, label = paste("\u2264 80% of holdings:\n", round(q80, digits = 2), "B")), size = 6/ggplot2::.pt) 
 
 # output figure
 output(
@@ -67,32 +67,32 @@ output(
 
 
 
-# calculate quantiles for the cumulative distribution function
+# calculate quantiles for the cumulative distribution function for S&P 1,500 composite equity index
 sp1500 <- equities %>%
   dplyr::group_by(Sector) %>%
   dplyr::mutate(
-    q50 = stats::quantile(aum, probs = 0.50), # calculate size of 50% of firms 
-    q80 = stats::quantile(aum, probs = 0.80) # calculate size of 80% of firms
+    q50 = stats::quantile(holdings, probs = 0.50), # 50% of holdings
+    q80 = stats::quantile(holdings, probs = 0.80) # 80% of holdings
     ) %>%
   dplyr::ungroup()
 
 # construct the plot
-fig02 <- ggplot2::ggplot(sp1500, ggplot2::aes(aum)) +
+fig02 <- ggplot2::ggplot(sp1500, ggplot2::aes(holdings)) +
   ggplot2::stat_ecdf(geom = "step", linewidth = 1, col = "black", alpha = 1.00) + # geom = "area"
   ggplot2::facet_wrap(~ Sector, nrow = 3, ncol = 4, scales = "fixed") +
   ggplot2::theme_bw() +
   ggplot2::theme(text = ggplot2::element_text(size = 12)) +
   ggplot2::scale_x_log10() +
   ggplot2::scale_y_continuous(labels = scales::percent) +
-  ggplot2::xlab("(Logged) Assets under management S&P 1,500 Composite, Billions (USD)") +
+  ggplot2::xlab("(Logged) Holdings in S&P 1,500 Composite, Billions (USD)") +
   ggplot2::ylab("Empiricial Cumulative Distribution Function (ECDF)") +
-  # mark 50% of firms
+  # mark 50% of holdings
   ggplot2::geom_vline(data = sp1500, ggplot2::aes(xintercept = q80), linewidth = 1, linetype = "dashed", color = "red") +
-  # mark 80% of firms
+  # mark 80% of holdings
   ggplot2::geom_vline(data = sp1500, ggplot2::aes(xintercept = q50), linewidth = 1, linetype = "dashed", color = "red") +
   # label the lines
-  ggplot2::geom_label(data = sp1500, mapping = ggplot2::aes(x = q50, y = 0.25, label = paste("\u2264 50% of firms:\n ", round(q50, digits = 2), "B")), size = 6/ggplot2::.pt) + 
-  ggplot2::geom_label(data = sp1500, mapping = ggplot2::aes(x = q80, y = 0.50, label = paste("\u2264 80% of firms:\n ", round(q80, digits = 2), "B")), size = 6/ggplot2::.pt) 
+  ggplot2::geom_label(data = sp1500, mapping = ggplot2::aes(x = q50, y = 0.25, label = paste("\u2264 50% of holdings:\n ", round(q50, digits = 2), "B")), size = 6/ggplot2::.pt) + 
+  ggplot2::geom_label(data = sp1500, mapping = ggplot2::aes(x = q80, y = 0.50, label = paste("\u2264 80% of holdings:\n ", round(q80, digits = 2), "B")), size = 6/ggplot2::.pt) 
 
 # output figure
 output(
@@ -105,13 +105,13 @@ output(
 
 
 
-# the cumulative distribution of firm size for S&P 500 large-cap equities
+# calculate quantiles for the cumulative distribution for S&P 500 large-cap equities
 sp500 <- equities %>%
   dplyr::filter(Index == "S&P 500") %>%
   dplyr::group_by(Sector) %>%
   dplyr::mutate(
-    q50 = stats::quantile(aum, probs = 0.50), # calculate size of 50% of firms 
-    q80 = stats::quantile(aum, probs = 0.80) # calculate size of 80% of firms
+    q50 = stats::quantile(holdings, probs = 0.50), # 50% of holdings
+    q80 = stats::quantile(holdings, probs = 0.80) # 80% of holdings
   ) %>%
   dplyr::ungroup()
 
@@ -123,15 +123,15 @@ fig03 <- ggplot2::ggplot(sp500, ggplot2::aes(aum)) +
   ggplot2::theme(text = ggplot2::element_text(size = 12)) +
   ggplot2::scale_x_log10()+
   ggplot2::scale_y_continuous(labels = scales::percent) +
-  ggplot2::xlab("(Logged) Assets under managementn S&P 500, Billions (USD)") +
+  ggplot2::xlab("(Logged) Holdings in S&P 500, Billions (USD)") +
   ggplot2::ylab("Empiricial Cumulative Distribution Function (ECDF)") +
-  # mark 50% of firms
+  # mark 50% of holdings
   ggplot2::geom_vline(data = sp500, ggplot2::aes(xintercept = q80), linewidth = 1, linetype = "dashed", color = "red") +
-  # mark 80% of firms
+  # mark 80% of holdings
   ggplot2::geom_vline(data = sp500, ggplot2::aes(xintercept = q50), linewidth = 1, linetype = "dashed", color = "red") +
   # label the lines
-  ggplot2::geom_label(data = sp500, mapping = ggplot2::aes(x = q50, y = 0.25, label = paste("\u2264 50% of firms:\n", round(q50, digits = 2), "B")), size = 6/ggplot2::.pt) + 
-  ggplot2::geom_label(data = sp500, mapping = ggplot2::aes(x = q80, y = 0.50, label = paste("\u2264 80% of firms:\n", round(q80, digits = 2), "B")), size = 6/ggplot2::.pt) 
+  ggplot2::geom_label(data = sp500, mapping = ggplot2::aes(x = q50, y = 0.25, label = paste("\u2264 50% of holdings:\n", round(q50, digits = 2), "B")), size = 6/ggplot2::.pt) + 
+  ggplot2::geom_label(data = sp500, mapping = ggplot2::aes(x = q80, y = 0.50, label = paste("\u2264 80% of holdings:\n", round(q80, digits = 2), "B")), size = 6/ggplot2::.pt) 
 
 # output figure
 output(
@@ -144,33 +144,33 @@ output(
 
 
 
-# the cumulative distribution of firm size for S&P 400 mid-cap equities
+# calculate the quantiles for the cumulative distribution of holdings for S&P 400 mid-cap equities
 mid_cap <- equities %>%
   dplyr::filter(Index == "S&P 400") %>%
   dplyr::group_by(Sector) %>%
   dplyr::mutate(
-    q50 = stats::quantile(aum, probs = 0.50), # calculate size of 50% of firms 
-    q80 = stats::quantile(aum, probs = 0.80) # calculate size of 80% of firms
+    q50 = stats::quantile(aum, probs = 0.50), # 50% of holdings
+    q80 = stats::quantile(aum, probs = 0.80) # 80% of holdings
   ) %>%
   dplyr::ungroup()
 
 # construct the plot
-fig04 <- ggplot2::ggplot(mid_cap, ggplot2::aes(aum)) +
+fig04 <- ggplot2::ggplot(mid_cap, ggplot2::aes(holdings)) +
   ggplot2::stat_ecdf(geom = "step", linewidth = 1, col = "black", alpha = 1.00) + # geom = "area"
   ggplot2::facet_wrap(~ Sector, nrow = 3, ncol = 4, scales = "fixed") +
   ggplot2::theme_bw() +
   ggplot2::theme(text = ggplot2::element_text(size = 12)) +
   ggplot2::scale_x_log10()+
   ggplot2::scale_y_continuous(labels = scales::percent) +
-  ggplot2::xlab("(Logged) Assets under management S&P 400, Billions (USD)") +
+  ggplot2::xlab("(Logged) Holdings in S&P 400, Billions (USD)") +
   ggplot2::ylab("Empiricial Cumulative Distribution Function (ECDF)") +
-  # mark 50% of firms
+  # mark 50% of holdings
   ggplot2::geom_vline(data = mid_cap, ggplot2::aes(xintercept = q80), linewidth = 1, linetype = "dashed", color = "red") +
-  # mark 80% of firms
+  # mark 80% of holdings
   ggplot2::geom_vline(data = mid_cap, ggplot2::aes(xintercept = q50), linewidth = 1, linetype = "dashed", color = "red") +
   # label the lines
-  ggplot2::geom_label(data = mid_cap, mapping = ggplot2::aes(x = q50, y = 0.25, label = paste("\u2264 50% of firms:\n", round(q50, digits = 2), "B")), size = 6/ggplot2::.pt) + 
-  ggplot2::geom_label(data = mid_cap, mapping = ggplot2::aes(x = q80, y = 0.50, label = paste("\u2264 80% of firms:\n", round(q80, digits = 2), "B")), size = 6/ggplot2::.pt) 
+  ggplot2::geom_label(data = mid_cap, mapping = ggplot2::aes(x = q50, y = 0.25, label = paste("\u2264 50% of holdings:\n", round(q50, digits = 2), "B")), size = 6/ggplot2::.pt) + 
+  ggplot2::geom_label(data = mid_cap, mapping = ggplot2::aes(x = q80, y = 0.50, label = paste("\u2264 80% of holdings:\n", round(q80, digits = 2), "B")), size = 6/ggplot2::.pt) 
 
 # output figure
 output(
@@ -183,33 +183,33 @@ output(
 
 
 
-# the cumulative distribution of firm size for S&P 600 small-cap equities
+# calculate quantiles for the the cumulative distribution of holdings for S&P 600 small-cap equities
 small_cap <- equities %>%
   dplyr::filter(Index == "S&P 600") %>%
   dplyr::group_by(Sector) %>%
   dplyr::mutate(
-    q50 = stats::quantile(aum, probs = 0.50), # calculate size of 50% of firms 
-    q80 = stats::quantile(aum, probs = 0.80) # calculate size of 80% of firms
+    q50 = stats::quantile(holdings, probs = 0.50), # 50% of holdings 
+    q80 = stats::quantile(holdings, probs = 0.80) # 80% of holdings
     ) %>%
   dplyr::ungroup()
 
 # construct the plot
-fig05 <- ggplot2::ggplot(small_cap, ggplot2::aes(aum)) +
+fig05 <- ggplot2::ggplot(small_cap, ggplot2::aes(holdings)) +
   ggplot2::stat_ecdf(geom = "step", linewidth = 1, col = "black", alpha = 1.00) + # geom = "area"
   ggplot2::facet_wrap(~ Sector, nrow = 3, ncol = 4, scales = "fixed") +
   ggplot2::theme_bw() +
   ggplot2::theme(text = ggplot2::element_text(size = 12)) +
   ggplot2::scale_x_log10()+
   ggplot2::scale_y_continuous(labels = scales::percent) +
-  ggplot2::xlab("(Logged) Assets under management S&P 600, Billions (USD)") +
+  ggplot2::xlab("(Logged) Holdings in S&P 600, Billions (USD)") +
   ggplot2::ylab("Empiricial Cumulative Distribution Function (ECDF)") +
-  # mark 50% of firms
+  # mark 50% of holdings
   ggplot2::geom_vline(data = small_cap, ggplot2::aes(xintercept = q80), linewidth = 1, linetype = "dashed", color = "red") +
-  # mark 80% of frims
+  # mark 80% of holdings
   ggplot2::geom_vline(data = small_cap, ggplot2::aes(xintercept = q50), linewidth = 1, linetype = "dashed", color = "red") +
   # label the lines
-  ggplot2::geom_label(data = small_cap, mapping = ggplot2::aes(x = q50, y = 0.25, label = paste("\u2264 50% of firms:\n", round(q50, digits = 2), "B")), size = 6/ggplot2::.pt) + 
-  ggplot2::geom_label(data = small_cap, mapping = ggplot2::aes(x = q80, y = 0.50, label = paste("\u2264 80% of firms:\n", round(q80, digits = 2), "B")), size = 6/ggplot2::.pt) 
+  ggplot2::geom_label(data = small_cap, mapping = ggplot2::aes(x = q50, y = 0.25, label = paste("\u2264 50% of holdings:\n", round(q50, digits = 2), "B")), size = 6/ggplot2::.pt) + 
+  ggplot2::geom_label(data = small_cap, mapping = ggplot2::aes(x = q80, y = 0.50, label = paste("\u2264 80% of holdings:\n", round(q80, digits = 2), "B")), size = 6/ggplot2::.pt) 
 
 # output figure
 output(
